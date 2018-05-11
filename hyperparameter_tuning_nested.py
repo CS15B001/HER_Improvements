b32aeca79ee8412abbdc0d783717b3be0910df4a
gsatallion8 <gsarma8@her-fetch-instance.c.tonal-edge-199107.internal>
2018-05-11 14:25:29 +0000

    Added log files

--- hyperparameter_tuning_nested.py
+++ hyperparameter_tuning_nested.py
@@ -5,10 +5,10 @@ import datetime
 
 # Mention the experiments that need to be run here
 time_of_run = str(datetime.datetime.fromtimestamp(time.time()).strftime('%m_%d_%H_%M_%S'))
-batch_size = [512, 256]
-replay_k = [4,6,8]
-env_name = ['FetchReach-v1', 'FetchSlide-v1']
-n_epochs = [3, 50]
+batch_size = [256, 512]
+replay_k = [6,8]
+env_name = ['FetchSlide-v1']
+n_epochs = [50]
 n_reps = 1
 num_cpus = 12
 
@@ -19,6 +19,6 @@ for env_i in range(len(env_name)):
 		print("Batch Size ", batch_size[batch_i])
 		for replay_i in range(len(replay_k)):
 			print("replay_k ", replay_k[replay_i])
-			os.system('nohup python3 -m baselines.her.experiment.train --num_cpu '+str(num_cpus)+' --env_name '+str(env_name[env_i])+ ' --n_epochs '+str(n_epochs[env_i]) \
+			os.system('nohup python3 -m baselines.her.experiment.train --num_cpu '+str(num_cpus)+' --env_name '+str(env_name[env_i])+' --logdir /home/gsarma8/HER_Improvements/'+' --n_epochs '+str(n_epochs[env_i]) \
 				+' --batch_size '+str(batch_size[batch_i])+' --n_reps '+str(n_reps)+' --replay_k '+str(replay_k[replay_i]) \
 				+' >log_file_'+str(env_name[env_i])+'_n_cycles_50_batch_size_'+str(batch_size[batch_i])+'_n_reps_'+str(n_reps)+'_replay_k_'+str(replay_k[replay_i])+'_'+time_of_run+'.txt 2>&1')
